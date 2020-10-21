fafe293be5838f1bde9f3df50b870e757b88473d
Luiz Filipe Machado Barni <machado@odahcam.com>
2020-10-20 23:26:26 -0300

    added all api items

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 // browser
-export * from './BrowserQRCodeReader';
-export * from './BrowserDatamatrixCodeReader';
-export * from './BrowserQRCodeSvgWriter';
+export * from './BrowserAztecCodeReader';
 export * from './BrowserBarcodeReader';
 export * from './BrowserCodeReader';
+export * from './BrowserDatamatrixCodeReader';
+export * from './BrowserMultiFormatReader';
+export * from './BrowserPDF417Reader';
+export * from './BrowserQRCodeReader';
+export * from './BrowserQRCodeSvgWriter';
 export * from './HTMLCanvasElementLuminanceSource';
 export * from './VideoInputDevice';
